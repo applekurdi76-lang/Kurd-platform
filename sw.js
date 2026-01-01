7d4e542ea1976e1aef36be14d99c4fee2c51ec1b
applekurdi76-lang <applekurdi76@gmail.com>
2026-01-01 08:45:24 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/login.html',
+                '/index.html',
+                '/manifest.json'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
